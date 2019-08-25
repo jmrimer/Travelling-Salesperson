cb0137efd197c08f47cb1fa4a6a7f6f4fd8e6ed5
Jason Rimer <jasonmrimer@gmail.com>
2019-08-24 22:01:41 -0400

    Walking Skeleton - Spring, Maven, React running from 8080

---
+++ classes/public/precache-manifest.9fd08e2651eea048645024322266f7b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1980b6b401b6de4cc2b38b8b90d64a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67829391ffceb56f128f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9c26d44c926e8bb0d0d9",
+    "url": "/static/js/2.d8983aff.chunk.js"
+  },
+  {
+    "revision": "67829391ffceb56f128f",
+    "url": "/static/js/main.106c7b26.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
