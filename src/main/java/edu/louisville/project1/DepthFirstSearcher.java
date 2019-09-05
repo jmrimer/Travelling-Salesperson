80d6a4c4726014a6530994aac1c87f94317b1414
Jason Rimer <jasonmrimer@gmail.com>
2019-09-05 13:12:21 -0400

    DFS works before refactor

--- src/main/java/edu/louisville/project1/DepthFirstSearcher.java
+++ src/main/java/edu/louisville/project1/DepthFirstSearcher.java
@@ -9,52 +9,12 @@ import java.util.*;
 public class DepthFirstSearcher {
   List<List<Node>> allPathsFromStartToEnd = new ArrayList<>();
 
-  List<Node> traverseGraphToEnd(Graph graph, Node start, Node end) {
+  List<List<Node>> traverseGraphToEnd(Graph graph, Node start, Node end) {
     Node rootNode = start;
     List<Node> traveledPath = new ArrayList<>();
-//    List<List<Node>> traveledPaths = new ArrayList<>(new ArrayList<>());
-
     traveledPath.add(start);
     recursiveExploration(graph, rootNode, end, traveledPath);
-
-//    traveledPath = exploreDepth(graph, rootNode, end);
-//    if (traveledPath.contains(end)) {
-//      traveledPaths.add(traveledPath);
-//    }
-//    find connected nodes
-//    break ties, take first node
-//    repeat until no children
-//    return one step, repeat until no children
-//    mark as discovered and visited
-    int bestPathLength = Integer.MAX_VALUE;
-    for (List<Node> path : this.allPathsFromStartToEnd) {
-      bestPathLength = Math.min(path.size(), bestPathLength);
-    }
-
-    Iterator<List<Node>> iterator = this.allPathsFromStartToEnd.iterator();
-    while (iterator.hasNext()) {
-      if (iterator.next().size() != bestPathLength) {
-        iterator.remove();
-      }
-    }
-
-    for (int i = 0; i < bestPathLength; i++) {
-      int lesserNodeValue = Integer.MAX_VALUE;
-      iterator = this.allPathsFromStartToEnd.iterator();
-      while (iterator.hasNext()) {
-        lesserNodeValue = Math.min(Integer.parseInt(iterator.next().get(i).getName()), lesserNodeValue);
-      }
-
-      iterator = this.allPathsFromStartToEnd.iterator();
-      while (iterator.hasNext()) {
-        if (Integer.parseInt(iterator.next().get(i).getName()) != lesserNodeValue) {
-          iterator.remove();
-        }
-      }
-    }
-
-    System.out.println(this.allPathsFromStartToEnd);
-    return this.allPathsFromStartToEnd.get(0);
+    return this.allPathsFromStartToEnd;
   }
 
   private void recursiveExploration(Graph graph, Node rootNode, Node end, List<Node> traveledPath) {
@@ -110,7 +70,33 @@ public class DepthFirstSearcher {
     return nodes != null;
   }
 
-  Deque<Node> findShortestPath(Graph graph, Node start, Node end) {
-    return new LinkedList<>();
+  List<Node> findShortestPath(List<List<Node>> allPathsFromStartToEnd) {
+    int bestPathLength = Integer.MAX_VALUE;
+    for (List<Node> path : allPathsFromStartToEnd) {
+      bestPathLength = Math.min(path.size(), bestPathLength);
+    }
+
+    Iterator<List<Node>> iterator = allPathsFromStartToEnd.iterator();
+    while (iterator.hasNext()) {
+      if (iterator.next().size() != bestPathLength) {
+        iterator.remove();
+      }
+    }
+
+    for (int i = 0; i < bestPathLength; i++) {
+      int lesserNodeValue = Integer.MAX_VALUE;
+      iterator = allPathsFromStartToEnd.iterator();
+      while (iterator.hasNext()) {
+        lesserNodeValue = Math.min(Integer.parseInt(iterator.next().get(i).getName()), lesserNodeValue);
+      }
+
+      iterator = allPathsFromStartToEnd.iterator();
+      while (iterator.hasNext()) {
+        if (Integer.parseInt(iterator.next().get(i).getName()) != lesserNodeValue) {
+          iterator.remove();
+        }
+      }
+    }
+    return allPathsFromStartToEnd.get(0);
   }
 }
--- src/test/java/edu/louisville/project1/DepthFirstSearcherTest.java
+++ src/test/java/edu/louisville/project1/DepthFirstSearcherTest.java
@@ -1,5 +1,6 @@
 package edu.louisville.project1;
 
+import edu.louisville.project1.graphs.Node;
 import org.junit.Test;
 
 import java.util.List;
@@ -10,16 +11,17 @@ public class DepthFirstSearcherTest extends BaseGraphSearchTest {
   @Test
   public void naivelyImplementsDFSTraversalOfAllPaths() {
     assertEquals(
-      16,
+      40,
       new DepthFirstSearcher().traverseGraphToEnd(graph, node1, node11).size()
     );
   }
 
   @Test
   public void calculatesShortestPath() {
+    List<List<Node>> allPaths = new DepthFirstSearcher().traverseGraphToEnd(graph, node1, node11);
     assertEquals(
       List.of(node1, node3, node5, node8, node11),
-      new DepthFirstSearcher().findShortestPath(graph, node1, node11)
+      new DepthFirstSearcher().findShortestPath(allPaths)
     );
   }
 }
