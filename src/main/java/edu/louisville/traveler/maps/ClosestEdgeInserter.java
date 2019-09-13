ee75bd739dee55227d8fe3705a9b13b7c555ec00
Jason Rimer <jasonmrimer@gmail.com>
2019-09-12 22:10:58 -0400

    Closest Edge Inserter refactored and ready to create end point

--- src/main/java/edu/louisville/traveler/maps/ClosestEdgeInserter.java
+++ src/main/java/edu/louisville/traveler/maps/ClosestEdgeInserter.java
@@ -6,39 +6,50 @@ import java.util.List;
 
 class ClosestEdgeInserter {
   private MapHelpers mapHelpers = new MapHelpers();
+  private HashSet<Edge> edges = new HashSet<>();
+  private List<City> route = new ArrayList<>();
+  private List<City> remainingCities;
+  private City lastCityVisited;
+  private double weight = 0;
 
-  WeightedRoute generateTourByInsertion(List<City> cities, City start) {
-    List<City> remainingCities = new ArrayList<>(cities);
-    List<City> route = new ArrayList<>(List.of(start));
-    HashSet<Edge> edges = new HashSet<>();
-    WeightedRoute weightedRoute = new WeightedRoute(route, 0);
+  WeightedRoute generateTour(List<City> cities, City start) {
+    remainingCities = new ArrayList<>(cities);
+    firstStopBasedOnPointDistanceFrom(start);
+    visitRemainingCities();
+    returnTo(start);
+    return new WeightedRoute(route, weight);
+  }
+
+  private void returnTo(City start) {
+    Edge returnToStart = new Edge(lastCityVisited, start);
+    route.add(start);
+    weight += returnToStart.getWeight();
+  }
+
+  private void visitRemainingCities() {
+    while (remainingCities.size() > 1) {
+      remainingCities.remove(lastCityVisited);
+      addJourneyLegToTour(
+        lastCityVisited,
+        mapHelpers.findNearestCity(edges, remainingCities)
+      );
+    }
+  }
+
+  private void firstStopBasedOnPointDistanceFrom(City start) {
+    route.add(start);
     remainingCities.remove(start);
+    addJourneyLegToTour(
+      start,
+      mapHelpers.findNearestCity(start, remainingCities)
+    );
+  }
 
-    City firstStop = mapHelpers.findNearestCity(start, remainingCities);
-    Edge edge1 = new Edge(start, firstStop);
-    edges.add(edge1);
-    weightedRoute.route.add(firstStop);
-    weightedRoute.setWeight(weightedRoute.getWeight() + edge1.getWeight());
-    remainingCities.remove(firstStop);
-
-
-    City secondStop = mapHelpers.findNearestCity(edges, remainingCities);
-    Edge edge2 = new Edge(firstStop, secondStop);
-    edges.add(edge2);
-    weightedRoute.route.add(secondStop);
-    weightedRoute.setWeight(weightedRoute.getWeight() + edge2.getWeight());
-    remainingCities.remove(secondStop);
-
-    City thirdStop = mapHelpers.findNearestCity(edges, remainingCities);
-    Edge edge3 = new Edge(secondStop, thirdStop);
-    edges.add(edge3);
-    weightedRoute.route.add(thirdStop);
-    weightedRoute.setWeight(weightedRoute.getWeight() + edge3.getWeight());
-    remainingCities.remove(thirdStop);
-
-    Edge edge4 = new Edge(thirdStop, start);
-    weightedRoute.route.add(start);
-    weightedRoute.setWeight(weightedRoute.getWeight() + edge4.getWeight());
-    return weightedRoute;
+  private void addJourneyLegToTour(City start, City end) {
+    Edge edge = new Edge(start, end);
+    edges.add(edge);
+    route.add(end);
+    weight += edge.getWeight();
+    lastCityVisited = end;
   }
 }
--- src/main/java/edu/louisville/traveler/maps/WeightedRoute.java
+++ src/main/java/edu/louisville/traveler/maps/WeightedRoute.java
@@ -2,11 +2,13 @@ package edu.louisville.traveler.maps;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 class WeightedRoute {
   List<City> route;
   double weight;
