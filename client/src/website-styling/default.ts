16cebceac7ed5fce4449520b28b465ff91642cd3
Jason Rimer <jasonmrimer@gmail.com>
2019-09-16 20:53:31 -0400

    Added animations for visual graphs and styled buttons across app

--- client/src/website-styling/default.ts
+++ client/src/website-styling/default.ts
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 export const theme = {
   background: 'linear-gradient(360deg,#1E222A 0%,#39414E 100%)',
   fontFamily: 'Righteous',
@@ -8,6 +10,24 @@ export const theme = {
     wedgewood: '#50819B',
     midnight: '#09132d',
     plum: '#230a27',
-    maroon5: '#6D2234'
+    maroon5: '#6D2234',
+    flare: '#C83A17'
   }
 };
+
+export const Button = styled.button`
+  display: inline-block;
+  font-family: Righteous, cursive;
+  font-size: 24px;
+  flex: 1;
+  background: ${(props) => props.theme.color.wedgewood};
+  border: 1px solid ${(props) => props.theme.color.foreground};
+  border-radius: 8px;
+  color: white;
+  padding: 16px;
+  cursor: pointer;
+  
+  :hover {
+    background: ${(props) => props.theme.color.lavender};
+  }
+`;
